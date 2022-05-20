9bf0156607407e7613113cead4daac47eb4d78b5
Kamil Sishev <darklektor1@gmail.com>
2022-05-21 02:29:04 +0500

    Delete state in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,7 @@ import Header from "@/components/Header";
 import Main from "@/components/Main";
 import Footer from "@/components/Footer";
 
-import { useState } from "react";
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App d-flex flex-column" style={{ minHeight: "100vh" }}>
       <Header />
