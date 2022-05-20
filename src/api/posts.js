431eb2905135931bcde1b97529cecc7834d2747e
Kamil Sishev <darklektor1@gmail.com>
2022-05-21 02:30:36 +0500

    Edit posts api

--- src/api/posts.js
+++ src/api/posts.js
@@ -5,7 +5,6 @@ const api_url = "https://dummyjson.com";
 export default {
   getPosts(page = 1) {
     const start = page === 1 ? 0 : (page - 1) * 10;
-
     const url = `${api_url}/posts?limit=10&skip=${start}`;
 
     return axios.get(url).then((res) => {
